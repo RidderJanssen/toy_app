7f2d44dfb6ff12335ba8da8f4e8b726b49694928
Ridder Janssen <ridderjanssen@gmail.com>
2017-07-25 18:29:47 +0200

    Finish Toy App

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def hello
-  	render html: 'Hello, world!'
+  def index
+  	render html: 'This is a toy app. Welcome! Go to /users to see the users and /microposts to see the microposts.'
   end
 end
